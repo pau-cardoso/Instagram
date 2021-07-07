6621cce25a51d808d30b3e4e9ad6e0c863018a0e
pau-cardoso <A01701490@itesm.mx>
2021-07-06 21:19:19 -0500

    Add logout

--- app/src/main/java/com/example/instapaulina/MainActivity.java
+++ app/src/main/java/com/example/instapaulina/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.instapaulina;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    Button btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogout = findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+            }
+        });
     }
 }
